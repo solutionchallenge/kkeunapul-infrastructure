d569036ba3b9af0f80a07be0e30479e024975255
revimal <seohho@gmail.com>
2025-02-23 19:20:15 +0900

    chore: fix variable referencing

--- local-runner.sh
+++ local-runner.sh
@@ -1,6 +1,6 @@
 terraform init \
-    -backend-config="bucket=${ENV_TFSTATE_BUCKET}" \
-    -backend-config="key=${ENV_TFSTATE_KEY}" \
-    -backend-config="endpoint=${ENV_TFSTATE_ENDPOINT}" \
+    -backend-config="bucket=${VAR_TFSTATE_BUCKET}" \
+    -backend-config="key=${VAR_TFSTATE_KEY}" \
+    -backend-config="endpoint=${VAR_TFSTATE_ENDPOINT}" \
     -backend-config="access_key=${SECRET_TFSTATE_ACCESS_KEY}" \
     -backend-config="secret_key=${SECRET_TFSTATE_SECRET_KEY}"
