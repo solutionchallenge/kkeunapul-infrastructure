a9aa8da168c29ece6ade84baaefd5bc5eaee3571
revimal <seohho@gmail.com>
2025-02-23 18:09:55 +0900

    feat: initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+# Manage 'tfstate' on the alternative provider. (for HA)
+terraform {
+  backend "s3" {
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 4"
+    }
+  }
+}
+
+provider "google" {
+    alias = "gcp-zone-a"
+    project = var.GCP_PROJECT_ID
+    region = var.GCP_REGION_ID
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "GCP_PROJECT_ID" {
+    type = string
+    default = "secret-sphinx-451807-d1"
+}
+
+variable "GCP_REGION_ID" {
+    type = string
+    default = "asia-northeast3-a"
+}
