2101c13c45d737c3ccd9dea4829f2732b3dfd49b
revimal <seohho@gmail.com>
2025-02-23 19:32:12 +0900

    fix: fix min_master_version of GKE

--- comupting-k8s.tf
+++ comupting-k8s.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "primary" {
   project                  = var.GCP_PROJECT_ID
   name                     = "${var.GCP_PROJECT_ID}-k8s-primary"
   location                 = var.GCP_REGION_ID
-  min_master_version       = "1.23"
+  min_master_version       = "1.30"
   remove_default_node_pool = true
   initial_node_count       = 1
   network                  = google_compute_network.vpc_main.name
