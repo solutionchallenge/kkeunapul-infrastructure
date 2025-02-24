d2a7d9e306782d671404bde74e29aa15a9d05168
revimal <seohho@gmail.com>
2025-02-25 02:00:54 +0900

    fix: remove 'config_context_cluster'

--- modules/gcp/kubernetes/provider.tf
+++ modules/gcp/kubernetes/provider.tf
@@ -6,7 +6,6 @@ data "google_container_cluster" "primary" {
 }
 
 provider "kubernetes" {
-  config_context_cluster = google_container_cluster.primary.name
   host                   = "https://${data.google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
