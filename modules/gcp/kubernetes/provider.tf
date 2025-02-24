16a3932fc40cd96c4745361ac98d09e70e486ee7
revimal <seohho@gmail.com>
2025-02-25 01:55:21 +0900

    chore: fix typos

--- modules/gcp/kubernetes/provider.tf
+++ modules/gcp/kubernetes/provider.tf
@@ -1,13 +1,13 @@
 data "google_client_config" "default" {}
 
 data "google_container_cluster" "primary" {
-  name     = "${google_container_cluster.primary.name}"
+  name     = google_container_cluster.primary.name
   location = var.GCP_REGION_ID
 }
 
 provider "kubernetes" {
-  config_context_cluster = "${google_container_cluster.primary.name}"
+  config_context_cluster = google_container_cluster.primary.name
   host                   = "https://${data.google_container_cluster.primary.endpoint}"
-  token                  = "${data.google_client_config.default.access_token}"
-  cluster_ca_certificate = "${base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
 }
