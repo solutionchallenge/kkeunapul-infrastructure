24b1ef82a770f0d359e21a56d750090deabcd621
revimal <seohho@gmail.com>
2025-02-25 17:44:00 +0900

    chore: log IP identifier

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "primary_ingress_ip" {
   value       = module.network.primary_ip_address
   description = "Kubernetes Ingress IP"
 }
+
+output "primary_ip_identifier" {
+  value       = module.network.primary_ip_name
+  description = "Kubernetes IP Identifier"
+}
