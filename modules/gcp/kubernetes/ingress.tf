ba1ba46bd50389c370362b8c6d45afc94786ba27
revimal <seohho@gmail.com>
2025-02-25 11:35:38 +0900

    feat: configure the NGINX proxy

--- modules/gcp/kubernetes/ingress.tf
+++ modules/gcp/kubernetes/ingress.tf
@@ -3,9 +3,19 @@ resource "kubernetes_ingress_v1" "primary" {
     name      = "ingress-primary"
     namespace = var.GCP_GKE_DEFAULT_NAMESPACE
     annotations = {
+      name                                          = "${var.GCP_GKE_DEFAULT_NAMESPACE}-ingress"
       "kubernetes.io/ingress.global-static-ip-name" = var.GCP_GKE_IP_NAME
     }
   }
-  spec {}
-  depends_on = [google_container_cluster.primary, kubernetes_namespace.default]
+  spec {
+    default_backend {
+      service {
+        name = "default-proxy"
+        port {
+          number = 80
+        }
+      }
+    }
+  }
+  depends_on = [kubernetes_service_v1.proxy]
 }
---
+++ modules/gcp/kubernetes/proxy.tf
@@ -0,0 +1,51 @@
+resource "kubernetes_deployment_v1" "proxy" {
+  metadata {
+    name      = "default-proxy"
+    namespace = var.GCP_GKE_DEFAULT_NAMESPACE
+    labels = {
+      app = "default-proxy"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "default-proxy"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "default-proxy"
+        }
+      }
+      spec {
+        container {
+          name  = "default-proxy"
+          image = "nginx"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+  depends_on = [kubernetes_namespace.default]
+}
+
+resource "kubernetes_service_v1" "proxy" {
+  metadata {
+    name      = "default-proxy"
+    namespace = var.GCP_GKE_DEFAULT_NAMESPACE
+  }
+  spec {
+    selector = {
+      app = "default-proxy"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+  depends_on = [kubernetes_deployment_v1.proxy]
+}
