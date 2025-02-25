93e12ed16d91b8babb07934a941d26b8117dad3c
revimal <seohho@gmail.com>
2025-02-25 11:57:29 +0900

    fix: remove purpose for EXTERNAL GLOBAL IP

--- modules/gcp/network/ip.tf
+++ modules/gcp/network/ip.tf
@@ -1,6 +1,5 @@
 resource "google_compute_global_address" "primary" {
   project      = var.GCP_PROJECT_ID
   name         = "${var.GCP_PROJECT_ID}-ip-primary"
-  purpose      = "VPC_PEERING"
   address_type = "EXTERNAL"
 }
