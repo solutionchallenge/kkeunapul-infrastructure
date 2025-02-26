d67fdd06cb44f1990dd0dac3bd5f62f94adbbf7f
revimal <seohho@gmail.com>
2025-02-26 22:04:34 +0900

    chore: change service port

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ module "echo" {
   SERVICE_REGION    = var.GCP_REGION_ID
   SERVICE_NAMESPACE = module.kubernetes.primary_cluster_namespace
   SERVICE_IMAGE     = "hashicorp/http-echo"
-  SERVICE_PORT      = 80
+  SERVICE_PORT      = 5678
   SERVICE_ARGS      = ["-text", "\"hello-world\""]
   SERVICE_REPLICAS  = 1
 }
