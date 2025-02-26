c50bb0bca144fde9c74da3a17b7767c395222781
revimal <seohho@gmail.com>
2025-02-26 11:42:27 +0900

    feat: add health-check

--- modules/gcp/service/main.tf
+++ modules/gcp/service/main.tf
@@ -5,6 +5,9 @@ resource "kubernetes_deployment_v1" "service" {
     labels = {
       app = var.SERVICE_NAME
     }
+    annotations = {
+      "cloud.google.com/neg" = "{\"ingress\": true}"
+    }
   }
   spec {
     replicas = var.SERVICE_REPLICAS
@@ -27,6 +30,32 @@ resource "kubernetes_deployment_v1" "service" {
           port {
             container_port = var.SERVICE_PORT
           }
+          resources {
+            requests = {
+              cpu    = var.SERVICE_CPU.0
+              memory = var.SERVICE_MEMORY.0
+            }
+            limits = {
+              cpu    = var.SERVICE_CPU.1
+              memory = var.SERVICE_MEMORY.1
+            }
+          }
+          liveness_probe {
+            http_get {
+              path = var.SERVICE_HEALTHCHECK_PATH
+              port = var.SERVICE_PORT
+            }
+            initial_delay_seconds = var.SERVICE_HEALTHCHECK_DELAY
+            period_seconds        = var.SERVICE_HEALTHCHECK_PERIOD
+          }
+          readiness_probe {
+            http_get {
+              path = var.SERVICE_HEALTHCHECK_PATH
+              port = var.SERVICE_PORT
+            }
+            initial_delay_seconds = var.SERVICE_HEALTHCHECK_DELAY
+            period_seconds        = var.SERVICE_HEALTHCHECK_PERIOD
+          }
         }
       }
     }
@@ -37,8 +66,12 @@ resource "kubernetes_service_v1" "service" {
   metadata {
     name      = var.SERVICE_NAME
     namespace = var.SERVICE_NAMESPACE
+    annotations = {
+      "cloud.google.com/neg" = "{\"ingress\": true}"
+    }
   }
   spec {
+    type = "NodePort"
     selector = {
       app = var.SERVICE_NAME
     }
--- modules/gcp/service/variables.tf
+++ modules/gcp/service/variables.tf
@@ -42,6 +42,36 @@ variable "SERVICE_EXPOSE" {
 
 variable "SERVICE_REPLICAS" {
   type        = number
-  default     = 3
+  default     = 1
   description = "Service Replicas"
+}
+
+variable "SERVICE_CPU" {
+  type        = tuple([string, string])
+  default     = ["500m", "500m"]
+  description = "Service CPU (Request, Limit)"
+}
+
+variable "SERVICE_MEMORY" {
+  type        = tuple([string, string])
+  default     = ["256Mi", "256Mi"]
+  description = "Service Memory (Request, Limit)"
+}
+
+variable "SERVICE_HEALTHCHECK_PATH" {
+  type        = string
+  default     = "/health"
+  description = "Service Health-check Path"
+}
+
+variable "SERVICE_HEALTHCHECK_DELAY" {
+  type        = number
+  default     = 5
+  description = "Service Health-check Delay (sec)"
+}
+
+variable "SERVICE_HEALTHCHECK_PERIOD" {
+  type        = number
+  default     = 5
+  description = "Service Health-check Period (sec)"
 }
