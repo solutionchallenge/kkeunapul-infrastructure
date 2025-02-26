6cf35ffc199aee576a4d0414b5e7551bbb1fbed0
revimal <seohho@gmail.com>
2025-02-26 22:38:25 +0900

    feat: add DNS provisioning

--- main.tf
+++ main.tf
@@ -56,6 +56,8 @@ module "echo" {
   SERVICE_NAMESPACE = module.kubernetes.primary_cluster_namespace
   SERVICE_IMAGE     = "hashicorp/http-echo"
   SERVICE_PORT      = 5678
-  SERVICE_ARGS      = ["-text", "\"hello-world\""]
+  SERVICE_ARGS      = ["-text", "hello-world"]
   SERVICE_REPLICAS  = 1
+  SERVICE_CPU       = ["200m", "200m"]
+  SERVICE_MEMORY    = ["128Mi", "128Mi"]
 }
