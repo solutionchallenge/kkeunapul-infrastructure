2ee86cb1c2ccb34e6fdad8af449cecef8aca5e0c
revimal <seohho@gmail.com>
2025-02-28 02:35:20 +0900

    chore: fill all services

--- modules/gcp/core/main.tf
+++ modules/gcp/core/main.tf
@@ -1,10 +1,13 @@
 resource "google_project_service" "enable" {
   for_each = toset([
-    "container.googleapis.com",
-    "compute.googleapis.com",
-    "artifactregistry.googleapis.com",
-    "certificatemanager.googleapis.com",
-    "cloudresourcemanager.googleapis.com"
+    "container.googleapis.com",            # Kubernetes Engine API
+    "compute.googleapis.com",              # Compute Engine API
+    "artifactregistry.googleapis.com",     # Artifact Registry API
+    "certificatemanager.googleapis.com",   # Certificate Manager API
+    "cloudresourcemanager.googleapis.com", # Cloud Resource Manager API
+    "monitoring.googleapis.com",           # Cloud Monitoring API
+    "logging.googleapis.com",              # Cloud Logging API
+    "serviceusage.googleapis.com"          # Service Usage API
   ])
   project = var.GCP_PROJECT_ID
   service = each.key
