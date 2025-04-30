ff88cc0744c7637a96a3287529cdf0a6d9a431d3
revimal <seohho@gmail.com>
2025-05-01 05:49:25 +0900

    chore: reformat DB user's password

--- gcp/modules/database/user.tf
+++ gcp/modules/database/user.tf
@@ -1,22 +1,22 @@
-resource "random_password" "db_user_passwords" {
+resource "random_password" "database" {
   for_each = { for k, v in var.GCP_DB_USERS : k => v if v.password == "" }
 
-  length  = 16
-  special = true
-  upper   = true
-  lower   = true
-  numeric = true
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+  upper            = true
+  lower            = true
+  numeric          = true
 
   keepers = {
-    user_name = each.value.name
+    user_key = each.key
   }
 }
 
 resource "google_sql_user" "global" {
   for_each = var.GCP_DB_USERS
-
-  project     = var.GCP_PROJECT_ID
-  name        = "${var.GCP_PROJECT_ID}-dba-${each.value.name}"
-  instance    = google_sql_database_instance.primary.name
-  password_wo = each.value.password != "" ? each.value.password : random_password.db_user_passwords[each.key].result
-} 
+  project  = var.GCP_PROJECT_ID
+  instance = google_sql_database_instance.primary.name
+  name     = "${var.GCP_PROJECT_ID}-dba-${each.value.name}"
+  password = each.value.password != "" ? each.value.password : random_password.database[each.key].result
+}
